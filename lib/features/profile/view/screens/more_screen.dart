cd03b49b3d721678b8a60aeea56fe13d9f735f1b
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-20 18:09:19 +0300

    Wire support and employee activity actions

--- lib/features/profile/view/screens/more_screen.dart
+++ lib/features/profile/view/screens/more_screen.dart
@@ -17,31 +17,31 @@ class MoreScreen extends StatelessWidget {
     return SafeArea(
       child: Column(
         children: [
-          MoreScreenAppBar(),
-          SizedBox(height: 16),
+          const MoreScreenAppBar(),
+          const SizedBox(height: 16),
           Expanded(
             child: SingleChildScrollView(
-              padding: EdgeInsetsDirectional.symmetric(horizontal: 24),
+              padding: const EdgeInsetsDirectional.symmetric(horizontal: 24),
               child: Column(
                 children: [
-                  SectionTitle(title: 'إعدادات المتجر'),
-                  SizedBox(height: 16),
+                  const SectionTitle(title: 'إعدادات المتجر'),
+                  const SizedBox(height: 16),
                   Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(24),
                       color: context.onPrimaryContainer,
-                      border: Border.all(color: Color(0xffF3F4F6), width: 1),
-                      boxShadow: [BoxShadow(color: Colors.black.withAlpha(6), offset: Offset(0, 2), blurRadius: 10)],
+                      border: Border.all(color: const Color(0xffF3F4F6), width: 1),
+                      boxShadow: [BoxShadow(color: Colors.black.withAlpha(6), offset: const Offset(0, 2), blurRadius: 10)],
                     ),
-                    padding: EdgeInsetsDirectional.all(16),
+                    padding: const EdgeInsetsDirectional.all(16),
                     child: Column(
                       children: [
                         BlocBuilder<ProfileBloc, ProfileState>(
                           builder: (context, state) {
                             final resturantData = state.resturantData?.data;
                             return SectionCard(
-                              containerColor: Color(0xffD1FAE5),
-                              imageColor: Color(0xff059669),
+                              containerColor: const Color(0xffD1FAE5),
+                              imageColor: const Color(0xff059669),
                               image: Assets.images.marketInfoIcon.path,
                               title: 'معلومات المتجر',
                               subtitle: 'الاسم والعنوان والتفاصيل',
@@ -52,126 +52,107 @@ class MoreScreen extends StatelessWidget {
                             );
                           },
                         ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.symmetric(vertical: 16),
-                          child: Divider(color: context.surface, thickness: .5),
-                        ),
+                        Padding(padding: const EdgeInsetsDirectional.symmetric(vertical: 16), child: Divider(color: context.surface, thickness: .5)),
                         SectionCard(
-                          containerColor: Color(0xffE0F2FE),
-                          imageColor: Color(0xff0284C7),
+                          containerColor: const Color(0xffE0F2FE),
+                          imageColor: const Color(0xff0284C7),
                           image: Assets.images.workingHourIcon.path,
                           title: 'ساعات العمل',
                           subtitle: 'تحديد اوقات الفتح والاغلاق',
-                          onTap: () {
-                            context.pushRoute('/workingtime');
-                          },
+                          onTap: () => context.pushRoute('/workingtime'),
                         ),
                       ],
                     ),
                   ),
-                  SizedBox(height: 16),
-                  SectionTitle(title: 'العروض والتسويق'),
-                  SizedBox(height: 16),
+                  const SizedBox(height: 16),
+                  const SectionTitle(title: 'العروض والتسويق'),
+                  const SizedBox(height: 16),
                   Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(24),
                       color: context.onPrimaryContainer,
-                      border: Border.all(color: Color(0xffF3F4F6), width: 1),
-                      boxShadow: [BoxShadow(color: Colors.black.withAlpha(6), offset: Offset(0, 2), blurRadius: 10)],
+                      border: Border.all(color: const Color(0xffF3F4F6), width: 1),
+                      boxShadow: [BoxShadow(color: Colors.black.withAlpha(6), offset: const Offset(0, 2), blurRadius: 10)],
                     ),
-                    padding: EdgeInsetsDirectional.all(16),
+                    padding: const EdgeInsetsDirectional.all(16),
                     child: Column(
                       children: [
                         SectionCard(
-                          containerColor: Color(0xffFEE2E2),
-                          imageColor: Color(0xffDC2626),
+                          containerColor: const Color(0xffFEE2E2),
+                          imageColor: const Color(0xffDC2626),
                           image: Assets.images.offersManagementIcon.path,
                           title: 'ادارة العروض',
                           subtitle: 'انشاء وتعديل العروض الترويجية',
-                          onTap: () {
-                            context.pushRoute('/offersmanagement');
-                          },
+                          onTap: () => context.pushRoute('/offersmanagement'),
                         ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.symmetric(vertical: 16),
-                          child: Divider(color: context.surface, thickness: .5),
-                        ),
+                        Padding(padding: const EdgeInsetsDirectional.symmetric(vertical: 16), child: Divider(color: context.surface, thickness: .5)),
                         SectionCard(
-                          containerColor: Color(0xffFEF3C7),
-                          imageColor: Color(0xffD97706),
+                          containerColor: const Color(0xffFEF3C7),
+                          imageColor: const Color(0xffD97706),
                           image: Assets.images.coponsIcon.path,
                           title: 'الكوبونات',
                           subtitle: 'ادارة اكواد الخصم',
-                          onTap: () {
-                            context.pushRoute('/couponsmanagement');
-                          },
+                          onTap: () => context.pushRoute('/couponsmanagement'),
                         ),
                       ],
                     ),
                   ),
-                  SizedBox(height: 16),
-                  SectionTitle(title: 'الموظفون والسجل'),
-                  SizedBox(height: 16),
+                  const SizedBox(height: 16),
+                  const SectionTitle(title: 'الموظفون والسجل'),
+                  const SizedBox(height: 16),
                   Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(24),
                       color: context.onPrimaryContainer,
-                      border: Border.all(color: Color(0xffF3F4F6), width: 1),
-                      boxShadow: [BoxShadow(color: Colors.black.withAlpha(6), offset: Offset(0, 2), blurRadius: 10)],
+                      border: Border.all(color: const Color(0xffF3F4F6), width: 1),
+                      boxShadow: [BoxShadow(color: Colors.black.withAlpha(6), offset: const Offset(0, 2), blurRadius: 10)],
                     ),
-                    padding: EdgeInsetsDirectional.all(16),
+                    padding: const EdgeInsetsDirectional.all(16),
                     child: Column(
                       children: [
                         SectionCard(
-                          containerColor: Color(0xffCFFAFE),
-                          imageColor: Color(0xff0891B2),
+                          containerColor: const Color(0xffCFFAFE),
+                          imageColor: const Color(0xff0891B2),
                           image: Assets.images.employeesManagementIcon.path,
                           title: 'إدارة الموظفين',
                           subtitle: 'إضافة وتعديل بيانات الموظفين',
-                          onTap: () {
-                            context.pushRoute('/employeesmanagement');
-                          },
+                          onTap: () => context.pushRoute('/employeesmanagement'),
                         ),
-                        Padding(
-                          padding: EdgeInsetsDirectional.symmetric(vertical: 16),
-                          child: Divider(color: context.surface, thickness: .5),
-                        ),
+                        Padding(padding: const EdgeInsetsDirectional.symmetric(vertical: 16), child: Divider(color: context.surface, thickness: .5)),
                         SectionCard(
-                          containerColor: Color(0xffF1F5F9),
-                          imageColor: Color(0xff475569),
+                          containerColor: const Color(0xffF1F5F9),
+                          imageColor: const Color(0xff475569),
                           image: Assets.images.employeesHistoryIcon.path,
                           title: 'سجل نشاط الموظفين',
                           subtitle: 'متابعة نشاط الفريق',
-                          onTap: () {
-                            // context.pushRoute('/couponsmanagement');
-                          },
+                          onTap: () => context.pushRoute('/employees/activity'),
                         ),
                       ],
                     ),
                   ),
-                  SizedBox(height: 16),
+                  const SizedBox(height: 16),
                   Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(24),
                       color: context.onPrimaryContainer,
-                      border: Border.all(color: Color(0xffF3F4F6), width: 1),
-                      boxShadow: [BoxShadow(color: Colors.black.withAlpha(6), offset: Offset(0, 2), blurRadius: 10)],
+                      border: Border.all(color: const Color(0xffF3F4F6), width: 1),
+                      boxShadow: [BoxShadow(color: Colors.black.withAlpha(6), offset: const Offset(0, 2), blurRadius: 10)],
                     ),
-                    padding: EdgeInsetsDirectional.all(16),
+                    padding: const EdgeInsetsDirectional.all(16),
                     child: Column(
                       children: [
                         SectionCard(
-                          containerColor: Color(0xffDBEAFE),
-                          imageColor: Color(0xff2563EB),
+                          containerColor: const Color(0xffDBEAFE),
+                          imageColor: const Color(0xff2563EB),
                           image: Assets.images.supportIcon.path,
                           title: 'الدعم الفني',
                           subtitle: 'تواصل مع فريق الدعم',
-                          onTap: () {},
+                          onTap: () => context.pushRoute('/support'),
                         ),
                       ],
                     ),
                   ),
-                  SizedBox(height: 12),
+                  const SizedBox(height: 12),
                   InkWell(
                     onTap: () {
                       SharedPreferencesHelper.clearData();
@@ -180,26 +161,26 @@ class MoreScreen extends StatelessWidget {
                     borderRadius: BorderRadius.circular(24),
                     child: Container(
                       decoration: BoxDecoration(
-                        color: Color(0xffEF4444).withAlpha(6),
+                        color: const Color(0xffEF4444).withAlpha(6),
                         borderRadius: BorderRadius.circular(24),
-                        border: Border.all(color: Color(0xffEF4444).withAlpha(52)),
+                        border: Border.all(color: const Color(0xffEF4444).withAlpha(52)),
                       ),
-                      padding: EdgeInsetsDirectional.symmetric(vertical: 16),
+                      padding: const EdgeInsetsDirectional.symmetric(vertical: 16),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Container(
-                            decoration: BoxDecoration(color: Color(0xffEF4444).withAlpha(25), borderRadius: BorderRadius.circular(16)),
-                            padding: EdgeInsetsDirectional.all(13),
-                            child: Icon(Icons.logout_rounded, color: Color(0xffEF4444)),
+                            decoration: BoxDecoration(color: const Color(0xffEF4444).withAlpha(25), borderRadius: BorderRadius.circular(16)),
+                            padding: const EdgeInsetsDirectional.all(13),
+                            child: const Icon(Icons.logout_rounded, color: Color(0xffEF4444)),
                           ),
-                          SizedBox(width: 12),
-                          AppText.bodyMedium('تسجيل الخروج', color: Color(0xffEF4444), fontWeight: FontWeight.bold),
+                          const SizedBox(width: 12),
+                          AppText.bodyMedium('تسجيل الخروج', color: const Color(0xffEF4444), fontWeight: FontWeight.bold),
                         ],
                       ),
                     ),
                   ),
-                  SizedBox(height: 16),
+                  const SizedBox(height: 16),
                 ],
               ),
             ),
